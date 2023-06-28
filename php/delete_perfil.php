0cc6e817a1a9f06b249d02b49e39d81962f8f5c0
José González <67356386+VoidRayXX@users.noreply.github.com>
2023-06-27 23:49:13 -0400

    fix

--- php/delete_perfil.php
+++ php/delete_perfil.php
@@ -10,6 +10,17 @@ try{
     $query = $base->prepare($sql);
     $query->bindValue(":username", $_SESSION["usuario"]);
     $query->execute();
+
+    $sql = "SELECT id FROM usuarios WHERE username = :username";
+    $query = $base->prepare($sql);
+    $query->bindValue(":username", $_SESSION["usuario"]);
+    $query->execute();
+    $id_user = $query->fetch()[0];
+
+    $sql = "DELETE FROM nombre_tabla WHERE id_contacto1 = :id OR id_contacto2 = :id";
+    $query = $base->prepare($sql);
+    $query->bindValue(":id", $id_user);
+    $query->execute();
     session_destroy();
     header("location: login.php");
 }
